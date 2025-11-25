a7c5c3ccf9f54261a7089f6191151928b5e0ce6e
Vinaykumar <gunjivinaykumar2001@gmail.com>
2025-11-25 18:38:11 +0530

    Fix Resend package name

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
-const { Resend } = require('@resend/node');
+const { Resend } = require('resend');
 const path = require('path');
 require('dotenv').config();
 
